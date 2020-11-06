33df8c239435e7102b8830a837509dcda9751834
devitasetyaningrum <ningrum.deviita@gmail.com>
2020-11-06 20:39:37 +0700

    Initial Commit

---
+++ UTSDevita/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UTSDevita
+//
+//  Created by Devita setyaningrum on 06/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
